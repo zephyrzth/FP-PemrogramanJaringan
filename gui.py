8cfadb37da4cd62683303e6bcf116728f3ad893b
JohnforJC <animeloverz072@gmail.com>
2020-05-20 22:54:14 +0700

    helper board

--- gui.py
+++ gui.py
@@ -30,14 +30,31 @@ def turn(event):
     if(playerTurn == 2): setPlayer(2)
     else: setPlayer(1)
 
+def hover(event):
+    
+    x = event.x_root - game_frame.winfo_rootx() 
+    y = event.y_root - game_frame.winfo_rooty()
+
+    z = game_frame.grid_location(x,y)
+
+    helperLabel.configure(image=HELPER_BOARD_IMAGE)
+    helperLabel.grid(column=z[0], row=z[1])
+    print( z )
+
+def out(event):
+    helperLabel.configure(image=NONE_BOARD_IMAGE)
 
 NONE_BOARD_LOCATION = "assets/none.png"
 WHITE_BOARD_LOCATION = "assets/white.png"
 BLACK_BOARD_LOCATION = "assets/black.png"
+HELPER_BOARD_LOCATION = "assets/helper.png"
 
 NONE_BOARD_IMAGE = inputImage(NONE_BOARD_LOCATION)
 WHITE_BOARD_IMAGE = inputImage(WHITE_BOARD_LOCATION)
 BLACK_BOARD_IMAGE = inputImage(BLACK_BOARD_LOCATION)
+HELPER_BOARD_IMAGE = inputImage(HELPER_BOARD_LOCATION)
+
+game_frame.bind("<Leave>", out)
 
 for i in range(8):
     for j in range(8):
@@ -48,18 +65,23 @@ for i in range(8):
                 image=NONE_BOARD_IMAGE,
             )
             label.bind("<Button-1>",turn)
-            label.grid(row=i, column=j)
+            label.bind("<Enter>", hover)
+            label.grid(column=i, row=j)
         elif(board_code[i][j]==1):
             label = Label(
                 game_frame,  
                 image=WHITE_BOARD_IMAGE
             )
-            label.grid(row=i, column=j)
+            label.grid(column=i, row=j)
+            label.bind("<Enter>", out)
         elif(board_code[i][j]==2):
             label = Label(
                 game_frame,  
                 image=BLACK_BOARD_IMAGE
             )
-            label.grid(row=i, column=j)
+            label.grid(column=i, row=j)
+            label.bind("<Enter>", out)
+
+helperLabel = Label(game_frame)
 
 window.mainloop()
