c84e684ac1ae3fa31b182f8bf6c4913df9833a02
irshadrasyidi <didot.brodot@yahoo.com>
2020-05-20 13:46:34 +0700

    init game class

--- server.py
+++ server.py
@@ -10,6 +10,23 @@ server_socket.listen(5)
 
 input_socket = [server_socket]
 
+gameID = 0
+# gameStatus List:
+# 0 exit
+# 1 play: player 1 turn
+# 2 play: player 2 turn
+# 3 finish
+
+class Game:
+    def __init__(self, gameID, player1, player2):
+        self.gameID = gameID
+        self.gameStatus = 1
+        self.player1 = player1
+        self.player2 = player2
+
+
+
+
 try:
     while True:
         read_ready, write_ready, exception = select.select(input_socket, [], [])
@@ -18,6 +35,8 @@ try:
             if sock == server_socket:
                 client_socket, client_address = server_socket.accept()
                 input_socket.append(client_socket)
+                gameID += 1
+                print(gameID)
             else:
                 data = sock.recv(1024).decode()
                 print(str(sock.getpeername()), str(data))
